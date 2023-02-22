a708e5cb12158fa39c62b1e2515464a40312be2f
alexanderZaych <118917232+Alexander-Zaychenko@users.noreply.github.com>
2023-02-22 16:30:15 +0000

    add commets

--- Hello, World.js
+++ Hello, World.js
@@ -1,6 +1,6 @@
 console.log('Hello, world!'); // comment
 console.log(1234)
-<DocumentTypeHtml>
-  <html>
-    alert("RED ALERT!!!")
-  </html>
+// <DocumentType html>
+//   <html>
+  //   alert("RED ALERT!!!")
+  // </html>
